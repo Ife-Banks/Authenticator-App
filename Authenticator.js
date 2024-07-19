fda5a0a5467681781157facd4334d208d0c1f621
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2024-07-19 22:34:46 +0100

    Disabled Cursor after 3 wrong trials

--- Authenticator.js
+++ Authenticator.js
@@ -3,7 +3,8 @@ do{
 let x = 3;
 function Authenticate() {
     var Password = document.getElementById("Password").value;
-    var y = document.getElementById("eqn");
+    var y = document.getElementsByClassName('input');
+    var inp = y[0];
     if(Password == "Bankole"){
         document.getElementById("Grant").innerHTML="Access Granted";
         
@@ -12,12 +13,17 @@ function Authenticate() {
     else{
         x-=1;
         if(x==0) {
-            alert("3 Wrong Input");
+            alert("Try Password After 1min");
+            inp.style.cursor ="not-allowed";
+            
             x = 3;
         }
         document.getElementById("denied").innerHTML="Access Denied Try again";
     }
     setTimeout(function(){document.getElementById("Grant").innerHTML="";},3000);
     setTimeout(function(){document.getElementById("denied").innerHTML="";},1000);
+    setTimeout(function(){
+        inp.style.cursor="default";
+    },60000);
 }
 }while(x>=0);
