8b103049336fc9903ae7b34e4f7394ef92fc86f9
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2024-07-19 22:43:31 +0100

    Disabled Entry Sucess

--- Authenticator.js
+++ Authenticator.js
@@ -4,6 +4,7 @@ let x = 3;
 function Authenticate() {
     var Password = document.getElementById("Password").value;
     var y = document.getElementsByClassName('input');
+    var b =document.getElementById('button');
     var inp = y[0];
     if(Password == "Bankole"){
         document.getElementById("Grant").innerHTML="Access Granted";
@@ -13,9 +14,9 @@ function Authenticate() {
     else{
         x-=1;
         if(x==0) {
-            alert("Try Password After 1min");
             inp.style.cursor ="not-allowed";
-            
+            b.style.display="none";
+            alert("Try Password After 30sec");
             x = 3;
         }
         document.getElementById("denied").innerHTML="Access Denied Try again";
@@ -24,6 +25,7 @@ function Authenticate() {
     setTimeout(function(){document.getElementById("denied").innerHTML="";},1000);
     setTimeout(function(){
         inp.style.cursor="default";
-    },60000);
+        b.style.display="block";
+    },30000);
 }
 }while(x>=0);
